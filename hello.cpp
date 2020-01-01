3ab386ef8443669fbdaaf13befa04c3e0838d75d
fizik1580 <48645575+fizik1580@users.noreply.github.com>
2020-01-02 00:16:01 +0300

    123

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char test_var[80];
+
+	cin >> test_var;
+
+	cout << "Hello world/it's c++/ and test var = " << test_var;
+
+	system("pause");
+	return 0;
+}
